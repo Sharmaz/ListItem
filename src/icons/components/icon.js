1e80a4d3d0493a52b6f28b858c67428d97a7dcc1
Ivan Robles <irae45@gmail.com>
2018-04-24 12:59:14 -0500

    Agregamos componentes para iconos que utilizaremos en los botones de las acciones de las listas

---
+++ src/icons/components/icon.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Icon = (props) => {
+  // Hacemos destructuring a las props color y size
+  const { color, size } = props;
+
+  return (
+    // Las propiedades se las pasamos al elemento SVG y rendereamos los hijos en el SVG
+    <svg
+      height={size}
+      width={size}
+      fill={color}
+      viewBox="0 0 32 32"
+    >
+      {props.children}
+    </svg>
+  );
+}
+
+export default Icon;
